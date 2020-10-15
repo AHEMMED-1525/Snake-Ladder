23ed38ec01db7cbf7393610be978b789c30efd75
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-15 23:13:21 +0530

    Dice Check

---
+++ Snake_Ladder.java
@@ -0,0 +1,14 @@
+public class Snake_Ladder{
+   public static void main(String[] args){
+      //Constants Declared
+
+         int Start_Pos=0;
+         int Win_Pos=100;
+      //Variables
+      int postion=0;
+		//Computation
+		int RndmChck=(int) Math.floor(Math.random()*10)%7;
+		System.out.println(RndmChck);
+   }
+}
+
